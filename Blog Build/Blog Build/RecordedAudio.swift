37709c802e6d67c91ee41e8634d7096887e977a8
JackScottie <eldridge.jack@gmail.com>
2015-04-06 22:02:32 -0700

    Committed by Udacity

---
+++ Blog Build/Blog Build/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Blog Build
+//
+//  Created by Jack on 3/27/15.
+//  Copyright (c) 2015 Jack. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
